2fab4169eac83b794a655dca0fda679c6b46b3a3
김범수 <bamtol5430@naver.com>
2021-08-27 23:04:01 +0900

    Edit MyModel to resnext50

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
 
 
 class BaseModel(nn.Module):
@@ -38,16 +39,22 @@ class BaseModel(nn.Module):
 class MyModel(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
+        self.pretrained = torchvision.models.resnext50_32x4d(pretrained=True)
+        self.pretrained.fc = nn.Sequential(
+            self.pretrained.fc,
+            nn.ReLU(inplace = True),
+            nn.Dropout(p = 0.3),
+            nn.Linear(1000, 256, bias = True),
+            nn.ReLU(inplace = True),
+            nn.Dropout(p = 0.3),
+            nn.Linear(256, num_classes, bias = True))
 
-        """
-        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
-        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
-        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
-        """
+        for layer in self.pretrained.fc:
+            if isinstance(layer, nn.Linear):
+                torch.nn.init.xavier_uniform_(layer.weight)
+                stdv = 1.0 / (layer.in_features ** 0.5)
+                layer.bias.data.uniform_(-stdv,stdv)
+
 
     def forward(self, x):
-        """
-        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
-        2. 결과로 나온 output 을 return 해주세요
-        """
-        return x
+        return self.pretrained(x)
